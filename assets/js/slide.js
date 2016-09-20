618db7242d5b821502fb074dc770db368d3145ab
emscape2 <emiel_silverentand@msn.com>
2016-09-20 22:43:49 +0200

    alle idioten
    die zingen over boten

---
+++ assets/js/slide.js
@@ -0,0 +1,71 @@
+
+window.onload = function () {
+
+    //staat 2x in het script
+    var biggestHeight = "0";
+// Loop through elements children to find & set the biggest height
+    $(".box").each(function () {
+
+        // If this elements height is bigger than the biggestHeight
+        if ($(this).height() > biggestHeight) {
+            // Set the biggestHeight to this Height
+            biggestHeight = $(this).height();
+        }
+    });
+
+    biggestHeight += 120;
+// Set the container height deze doet zijn werk vanwege timingsissues nog niet goed
+    $("#sliderwrapper").height(biggestHeight);
+
+
+
+
+    $('.box').click(function () {
+        $('.box').each(function () {
+            if ($(this).offset().left < 0) {
+                $(this).css("left", "200%");
+            }
+        });
+
+        $(this).animate({
+            left: '-100%'
+        }, 500);
+
+        if ($(this).next().size() > 0) {
+            $(this).next().animate({
+                left: '0%'
+            }, 500);
+        } else {
+            $(this).prevAll().last().animate({
+                left: '0%'
+            }, 500);
+        }
+
+    })
+
+
+    ;
+
+    window.onresize= function () {
+
+        var biggestHeight = "0";
+// Loop through elements children to find & set the biggest height
+        $(".box").each(function () {
+
+            // If this elements height is bigger than the biggestHeight
+            if ($(this).height() > biggestHeight) {
+                // Set the biggestHeight to this Height
+                biggestHeight = $(this).height();
+            }
+        });
+
+        biggestHeight += 120;
+// Set the container height deze doet zijn werk vanwege timingsissues nog niet goed
+        $("#sliderwrapper").height(biggestHeight);
+
+    };
+
+
+}/**
+ * Created by Emscape on 17-9-2016.
+ */
